c5886012572280f5f13a3aaab1b42886d472c0c2
santosh <santosh.ece06@gmail.com>
2021-06-03 10:47:19 +0530

    create mployee form desgin

--- src/components/EmployeeUpsert.js
+++ src/components/EmployeeUpsert.js
@@ -1,7 +1,67 @@
 export function EmployeeUpsert() {
   return (
-    <div>
-      <h1>Create Employee/ Update Employee Page</h1>
+    <div className="row">
+      <div className="col-3 col-md-3 d-none d-md-block"></div>
+      <div className="col-12 col-md-6">
+        <h3 className="alert alert-secondary">Crate Employee</h3>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter First name"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Lastname"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Username"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter Password"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Email"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Mobile"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="button"
+            className="btn btn-secondary w-100"
+            value="Add Employee"
+          />
+        </div>
+      </div>
+      <div className="col-3 col-md-3  d-none d-md-block"></div>
     </div>
   );
 }
