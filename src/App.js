386b780f32b47880920ba07f823da65ea569a51f
santosh <santosh.ece06@gmail.com>
2021-06-03 10:04:30 +0530

    routintg pages created.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { EmployeeUpsert } from "./components/EmployeeUpsert";
+import { EmployeeList } from "./components/EmployeeList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-employee">
+          <EmployeeUpsert />
+        </Route>
+
+        <Route path="/list-employee">
+          <EmployeeList />
+        </Route>
+
+        <Route exact path="/">
+          <EmployeeList />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
