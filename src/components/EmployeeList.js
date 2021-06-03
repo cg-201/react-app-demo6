769408ef3a303b5c2708de53987e51dd0eb53705
santosh <santosh.ece06@gmail.com>
2021-06-03 10:35:16 +0530

    fetching teh data in component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+
 export function EmployeeList() {
+  const state = useSelector((state) => state);
+  console.log(state);
+
   return (
     <div>
       <h1>Employee Listing Page</h1>
+
+      <h3>Sample List</h3>
+      {state.employee.sampleList.map((item, index) => (
+        <div key={index} className="alert alert-secondary">
+          {item}
+        </div>
+      ))}
     </div>
   );
 }
